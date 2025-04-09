d93123e50a4e44da02c423ac16e267f573bf2111
ROBLOXHACCKER <emailcreatasulmomentoo@gmail.com>
2025-04-09 14:17:04 +0200

    add: Draw Lines

---
+++ 3 - Draw Lines/src/main.c
@@ -0,0 +1,45 @@
+#include "../include/utils.h"
+
+
+int main(void)
+{
+    void *mlx;
+    void *win;
+    void *img;
+    int *data;
+    int width = 800;
+    int height = 600;
+    //int grid_width = 10;
+    int grid_tomargin = 50;
+    int widthlinee = 6;
+    int tot_pixel = width * height;
+    int temp_1, temp_2, temp_3;
+    int i = 0;
+    int x = 0;
+    char *title = "so_long"; 
+    
+
+    mlx = mlx_init();
+    win = mlx_new_window(mlx, width, height, title);
+    img = mlx_new_image(mlx, width, height);
+    data = (int*)mlx_get_data_addr(img, &temp_1, &temp_2, &temp_3);
+    
+    while(x < tot_pixel)
+    {
+        if(i > (grid_tomargin - (widthlinee/2)) && i < (grid_tomargin + (widthlinee/2))) 
+        {
+            data[x] = 0xFFA500;
+            printf("%d", i);
+        }
+        if(i%(grid_tomargin + (widthlinee/2))==0 || x%width==0)
+        {
+            i = 0;
+        }
+        i++;
+        x++;
+    }
+
+    mlx_put_image_to_window(mlx, win, img, 0, 0);
+
+    mlx_loop(mlx);
+}
