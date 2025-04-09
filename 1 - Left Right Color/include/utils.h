2a7a22132d865e88056731b05927e45dfc39da9a
ROBLOXHACCKER <emailcreatasulmomentoo@gmail.com>
2025-04-09 14:27:33 +0200

    added Left Right, rename projects

---
+++ 1 - Left Right Color/include/utils.h
@@ -0,0 +1,6 @@
+#include "./minilibx_opengl_20191021/mlx.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
---
+++ 1 - Left Right Color/src/main.c
@@ -0,0 +1,42 @@
+#include "../include/utils.h"
+
+
+int main(void)
+{
+    void *mlx;
+    void *win;
+    void *img;
+    int *data;
+    int width = 800;
+    int height = 600;
+    int tot_pixel = width * height;
+    int temp_1, temp_2, temp_3;
+    int i = 0;
+    int x = 0;
+    char *title = "so_long"; 
+
+    mlx = mlx_init();
+    win = mlx_new_window(mlx, width, height, title);
+    img = mlx_new_image(mlx, width, height);
+    data = (int*)mlx_get_data_addr(img, &temp_1, &temp_2, &temp_3);
+    while(x < tot_pixel)
+    {
+        while(i<width/2)
+        {
+            data[x] = 0x00FF0000;
+            i++;
+            x++;
+        }
+        while (i >= (width/2) && i <= (width-1))
+        {
+            data[x] = 0x00FF00FF;
+            i++;
+            x++;
+        }
+        i = 0;
+    }
+
+    mlx_put_image_to_window(mlx, win, img, 0, 0);
+
+    mlx_loop(mlx);
+}
