01e155ccc8efc1b833e248bbb0dbe2d15d5576d8
ROBLOXHACCKER <emailcreatasulmomentoo@gmail.com>
2025-04-09 17:34:37 +0200

    remove .a .o

--- 5 - Handle Keys Exit/src/main.c
+++ 5 - Handle Keys Exit/src/main.c
@@ -8,7 +8,8 @@ typedef struct
 
 int handle_key(int key, var_t *data)
 {
-    printf("[!] - keycode %d\n", key);
+    printf("keycode premuto: %d\n", key);
+
     if(key == 53)
     {
         mlx_destroy_window(data->mlx, data->win);
@@ -20,24 +21,18 @@ int handle_key(int key, var_t *data)
 
 int main(void)
 {
+
     var_t data;
     int width = 600;
-    int height = 400;
+    int height = 500;
     char *title = "so_long";
 
-
-
     data.mlx = mlx_init();
-    data.win = mlx_new_window(data.mlx, width, height, title);
+    data.win = mlx_new_window(data.mlx, width,height, title);
     mlx_key_hook(data.win, handle_key, &data);
-
-
     mlx_loop(data.mlx);
 
-
-
-
-
 }
+
 
 
