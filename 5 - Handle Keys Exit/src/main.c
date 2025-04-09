9947f0ffc6a3656ac01e9728dde5d02c9565b241
ROBLOXHACCKER <emailcreatasulmomentoo@gmail.com>
2025-04-09 17:22:49 +0200

    add Handle Key EXit

---
+++ 5 - Handle Keys Exit/src/main.c
@@ -0,0 +1,43 @@
+#include "../include/utils.h"
+
+typedef struct
+{
+    void *mlx;
+    void *win;
+}var_t;
+
+int handle_key(int key, var_t *data)
+{
+    printf("[!] - keycode %d\n", key);
+    if(key == 53)
+    {
+        mlx_destroy_window(data->mlx, data->win);
+        exit(0);
+    }
+    return 0;
+}
+
+
+int main(void)
+{
+    var_t data;
+    int width = 600;
+    int height = 400;
+    char *title = "so_long";
+
+
+
+    data.mlx = mlx_init();
+    data.win = mlx_new_window(data.mlx, width, height, title);
+    mlx_key_hook(data.win, handle_key, &data);
+
+
+    mlx_loop(data.mlx);
+
+
+
+
+
+}
+
+
