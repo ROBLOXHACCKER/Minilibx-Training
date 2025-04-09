b37ac8279826f4d3b02ac774a64df57580db3412
ROBLOXHACCKER <emailcreatasulmomentoo@gmail.com>
2025-04-09 16:21:02 +0200

    add griglia

--- 4 - Draw Lines/src/main.c
+++ 4 - Draw Lines/src/main.c
@@ -1,6 +1,5 @@
 #include "../include/utils.h"
 
-
 int main(void)
 {
     void *mlx;
@@ -9,7 +8,6 @@ int main(void)
     int *data;
     int width = 800;
     int height = 600;
-    //int grid_width = 10;
     int grid_tomargin = 50;
     int widthlinee = 6;
     int tot_pixel = width * height;
@@ -17,36 +15,34 @@ int main(void)
     int i = 0;
     int x = 0;
     int n = 0;
-    //int cl = 1;
+    //int y = 0;
     char *title = "so_long"; 
-    
 
     mlx = mlx_init();
     win = mlx_new_window(mlx, width, height, title);
     img = mlx_new_image(mlx, width, height);
     data = (int*)mlx_get_data_addr(img, &temp_1, &temp_2, &temp_3);
-    
-    while(x < tot_pixel)
+
+    while (x < tot_pixel)
     {
-        if((i > (grid_tomargin - (widthlinee/2)) && i < (grid_tomargin + (widthlinee/2))) || (x > (width * (grid_tomargin-(widthlinee/2))) && (x < (width * (grid_tomargin+(widthlinee/2)))) && (n > (grid_tomargin - (widthlinee/2))) && (n < (grid_tomargin + (widthlinee/2))))) 
+        if ((i % grid_tomargin >= grid_tomargin - widthlinee / 2) &&
+            (i % grid_tomargin <= grid_tomargin + widthlinee / 2))
         {
             data[x] = 0xFFA500;
-            printf("%d", i);
         }
-        
-        if(i%(grid_tomargin + (widthlinee/2))==0 || x%width==0)
+        else if ((n % grid_tomargin >= grid_tomargin - widthlinee / 2) &&
+                 (n % grid_tomargin <= grid_tomargin + widthlinee / 2))
         {
-            i = 0;
-        }
-        if(x%width == 0)
-        {
-            n++;
+            data[x] = 0xFFA500;
         }
         i++;
         x++;
+        if (i % width == 0)
+        {
+            i = 0;
+            n++;
+        }
     }
-
     mlx_put_image_to_window(mlx, win, img, 0, 0);
-
     mlx_loop(mlx);
 }
